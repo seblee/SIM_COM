d109ae53c797ba855c85d80ddcc9e4b294b99140
seblee <ceeu@vip.qq.com>
2018-10-15 18:02:26 +0800

    consummate SIMCOM_CCH operation;

--- SIMCOM_GPRS.c
+++ SIMCOM_GPRS.c
@@ -20,7 +20,9 @@
 /* Private typedef -----------------------------------------------------------*/
 
 /* Private define ------------------------------------------------------------*/
-
+#ifndef SIMCOM_CCH_log
+#define SIMCOM_CCH_log(N, ...) uart_printf("####[SIMCOM_CCH %s:%4d] " N "\r\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#endif /* at_log(...) */
 /* Private macro -------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
@@ -43,72 +45,47 @@
 bool SIMCOM_CCH(SIMCOM_HANDLE *pHandle, const char *host, int port)
 {
     bool result;
+    char buff[200];
     result = SIMCOM_SetParametersReturnBool(pHandle, "AT+CCHMODE=1", SIMCOM_DEFAULT_RETRY, 200, "\r\n设置SIM7000C CCHMODE 透传模式失败!\r\n");
-    if (result == FALSE)
+    if ((bool)result == FALSE)
         return result;
-    result = SIMCOM_SetParametersReturnBool(pHandle, "AT+CCHSTART", SIMCOM_DEFAULT_RETRY, 200, "\r\n设置SIM7000C CCH 启动失败!\r\n");
-    if (result == FALSE)
-        return result;
+    SIMCOM_CCH_log("设置透传模式成功");
 
-    SIMCOM_SendAT(pHandle, "AT+CGMM");
+    result = SIM900_CCHSTART(pHandle);
+    if ((bool)result == FALSE)
+        return result;
+    SIMCOM_CCH_log("启动通道成功");
+
+    rt_snprintf(buff, sizeof(buff), "AT+CCHOPEN=0,\"%s\",%d,2", host, port);
+    SIMCOM_CCH_log("buff:%s", buff);
+    SIMCOM_SendAT(pHandle, buff);
     pHandle->pClearRxData();
-    if (AT_RETURN_OK == SIMCOM_GetATResp(pHandle, &pData, &cnt, "OK", 10, 200)) //等待响应,超时200MS
+    //CONNECT 115200
+    if (AT_RETURN_OK == SIMCOM_GetATResp(pHandle, &pData, &cnt, "CONNECT", 10, 200)) //等待响应,超时200MS
     {
-        if (strstr((char *)pData, "SIM900") != NULL)
-            return SIMCOM_SIM900;
-        else if (strstr((char *)pData, "SIM800") != NULL)
-            return SIMCOM_SIM800;
-        else if (strstr((char *)pData, "SIM2000") != NULL)
-            return SIMCOM_SIM2000;
-        else if (strstr((char *)pData, "SIM7600") != NULL)
-            return SIMCOM_SIM7600;
-        else if (strstr((char *)pData, "SIMCOM_SIM868") != NULL)
-            return SIMCOM_SIM868;
-        else if (strstr((char *)pData, "SIMCOM_SIM7000C") != NULL)
-            return SIMCOM_SIM7000C;
-        else if (strstr((char *)pData, "LYNQ_L700") != NULL)
-            return LYNQ_L700;
-        else
-        {
-            uart_printf("未知通信模块：%s\r\n", pData);
-            return SIMCOM_INVALID;
-        }
+        SIMCOM_CCH_log("CONNECT 115200");
+        result = TRUE;
     }
+    else
+        result = FALSE;
+    return result;
 }
-
-/*************************************************************************************************************************
-* 函数				:	SIMCOM_NETSTATUS SIM900_SetGSM_CCHMODE(SIMCOM_HANDLE *pHandle)
-* 功能				:	获取GSM网络注册状态
-* 参数				:	pHandle:句柄
-* 返回				:	bool
-* 依赖				:	底层
-* 作者				:	cp1300@139.com
-* 时间				:	2013-10-29
-* 最后修改时间 		: 	2018-03-24
-* 说明				: 	当网络注册后,可能被拒绝,如果被拒绝,获取网络注册状态会提示
-						注册成功的,但是通过发送AT 后再去查询,会发现网络注册失败
-*************************************************************************************************************************/
-
 /**
  ****************************************************************************
- * @Function : void SIM900_CCHSTART(SIMCOM_HANDLE*pHandle)
+ * @Function : bool SIM900_CCHSTART(SIMCOM_HANDLE*pHandle)
  * @File     : SIMCOM_GPRS.c
  * @Program  : :handle
  * @Created  : 2018-10-09 by seblee
  * @Brief    : 
  * @Version  : V1.0
 **/
-void SIM900_CCHSTART(SIMCOM_HANDLE *pHandle)
-{
-}
-
 bool SIM900_CCHSTART(SIMCOM_HANDLE *pHandle)
 {
     u32 cnt;
     char *p;
     u8 retry = SIMCOM_DEFAULT_RETRY; //重试次数
     u8 *pData;
-    SIMCOM_NETSTATUS status;
+    int status;
 
     do
     {
@@ -120,9 +97,10 @@ bool SIM900_CCHSTART(SIMCOM_HANDLE *pHandle)
             p = strstr((const char *)pData, "+CCHSTART:"); //搜索字符"+CCHSTART:"
             if (p != NULL)                                 //搜索成功
             {
-                status = (SIMCOM_NETSTATUS)GSM_StringToDec(&p[11], 1);
+                status = GSM_StringToDec(&p[11], 1);
                 SIMCOM_TestAT(pHandle, 1);
-                return status;
+                if (status == 0)
+                    return TRUE;
             }
             break;
         }
@@ -133,5 +111,5 @@ bool SIM900_CCHSTART(SIMCOM_HANDLE *pHandle)
     } while (retry);
 
     SIMCOM_TestAT(pHandle, 2);
-    return SIMCOM_NET_ERROR;
+    return FALSE;
 }
